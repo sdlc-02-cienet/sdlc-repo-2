3600e7b8ee3d924ca2c45f8f98c8d0247c56769e
SDLC01 <sdlc.01.cienet@gmail.com>
2025-01-26 20:50:34 -0500

    Extend add_numbers to Summation of a List

--- utils.py
+++ utils.py
@@ -3,10 +3,20 @@ utils.py
 A collection of helper functions.
 """
 
-def add_numbers(a: float, b: float) -> float:
+def add_numbers(a, b=None):
     """
-    Returns the sum of two numbers.
+    Returns the sum of either:
+      - Two numbers: add_numbers(1.0, 2.0)
+      - A list of numbers: add_numbers([1.0, 2.0, 3.0])
     """
+    if isinstance(a, list):
+        # If 'a' is actually a list, just sum its items
+        return sum(a)
+
+    # If 'a' isn't a list, assume we have two separate numbers
+    if b is None:
+        raise ValueError("Second argument must be provided if the first isn't a list.")
+
     return a + b
 
 def greet(name: str) -> str:
